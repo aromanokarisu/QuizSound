760ab3f70b29b712212577c750452dabc4c2deb9
nanamiakri <1225chiruchiru805282@gmail.com>
2018-09-23 21:58:49 +0900

    初コミット

---
+++ Assets/Scripts/AdMod.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdMod : MonoBehaviour {
+
+	void Start () {
+        string appId = "ca-app-pub-6510313420673654~3050732925";
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+        RequestBanner();
+	}
+
+    private void RequestBanner(){
+        string adUnitId = "ca-app-pub-6510313420673654/2034940823";
+        // Create a 320x50 banner at the top of the screen.
+        BannerView bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the banner with the request.
+        bannerView.LoadAd(request);
+        // Create a 320x50 banner at the top of the screen.
+        //bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+    }
+}
---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour {
+
+	public AudioClip audioMaru;
+	public AudioClip audioBastsu;
+	private AudioSource audioSource;
+
+	public void Start () {
+		this.audioSource = gameObject.GetComponent<AudioSource>();
+	}
+
+	public void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+		    string objName = getTouchObjName();
+			switch (objName) {
+				case "Maru":
+					audioSource.clip = this.audioMaru;
+					audioSource.Play();
+					break;
+				case "Batsu":
+					audioSource.clip = this.audioBastsu;
+					audioSource.Play();
+					break;
+				default:
+					break;
+			}
+	　	}
+	}
+
+	/// <summary>
+	/// タッチされたオブジェクト名取得
+	/// </summary>
+	/// <returns>オブジェクト名 ない場合は空文字</returns>
+	private string getTouchObjName () {
+	　　Vector3 aTapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	　　Collider2D aCollider2d = Physics2D.OverlapPoint(aTapPoint);
+	　　if (aCollider2d) {
+	　　　	GameObject obj = aCollider2d.transform.gameObject;
+	　　　	return obj.name;
+	　　}
+		return "";
+	}
+}
